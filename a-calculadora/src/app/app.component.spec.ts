79a629dcba7eba9d1b975ec86bb2084ffc26edd9
ssj4dofuturo <felip3.fl@gmail.com>
2021-09-18 17:33:33 -0300

    Remocao codigo desnecessarios

--- a-calculadora/src/app/app.component.spec.ts
+++ a-calculadora/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'a-calculadora'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('a-calculadora');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('a-calculadora app is running!');
-  });
 });
