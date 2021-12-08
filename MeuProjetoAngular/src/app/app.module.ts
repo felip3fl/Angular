bf8fd200b693c656dc0de277bc330a2ce38c1b1f
ssj4dofuturo <felip3.fl@gmail.com>
2021-12-08 17:15:33 -0300

    Criacao componentes institucional

--- MeuProjetoAngular/src/app/app.module.ts
+++ MeuProjetoAngular/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
