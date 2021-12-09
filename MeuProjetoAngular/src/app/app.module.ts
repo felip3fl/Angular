615012d79594ebdb3f02c1769f02b86ca341f8b9
ssj4dofuturo <felip3.fl@gmail.com>
2021-12-09 13:41:43 -0300

    DataBind

--- MeuProjetoAngular/src/app/app.module.ts
+++ MeuProjetoAngular/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { DataBindingComponent } from './demos/data-binding/data-binding.componen
   ],
   imports: [
     BrowserModule,
-    [RouterModule.forRoot(rootRouterConfig, {useHash:false})]
+    [RouterModule.forRoot(rootRouterConfig, {useHash:false})],
+    FormsModule
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: "/"}
--- MeuProjetoAngular/src/app/demos/data-binding/data-binding.component.ts
+++ MeuProjetoAngular/src/app/demos/data-binding/data-binding.component.ts
@@ -7,4 +7,20 @@ import { Component, OnInit } from '@angular/core';
 
 export class DataBindingComponent  {
 
+  public contadorClique: number = 0;
+  public urlImagem: string = "https://angular.io/assets/images/logos/angular/angular.svg";
+  public nome: string = "";
+
+  adicionarClique(){
+    this.contadorClique++;
+  }
+
+  zerarContador(){
+    this.contadorClique = 0;
+  }
+
+  /* KeyUp(event: any){
+    this.nome = event.target.value;
+  } */
+
 }
