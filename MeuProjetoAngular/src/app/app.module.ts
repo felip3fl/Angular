0c14d9ab8932bc03191b8ff1127ba7493a9091c2
ssj4dofuturo <felip3.fl@gmail.com>
2021-12-09 09:39:00 -0300

    Roteamento e configuração

--- MeuProjetoAngular/src/app/app.module.ts
+++ MeuProjetoAngular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -7,6 +9,7 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,12 @@ import { ContatoComponent } from './institucional/contato/contato.component';
     ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash:false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: "/"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MeuProjetoAngular/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { ContatoComponent } from "./institucional/contato/contato.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
+import { HomeComponent } from "./navegacao/home/home.component";
+
+export const rootRouterConfig: Routes = [
+    {path: '', redirectTo: '/home', pathMatch:'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent },
+    { path: 'sobre', component: SobreComponent }
+    // { path: 'feature-data-binding', component: DataBindingComponent },
+    // { path: 'produtos', component: ListaProdutoComponent },
+    // { path: 'produto-detalhe/:id', component: ListaProdutoComponent }];
+];
