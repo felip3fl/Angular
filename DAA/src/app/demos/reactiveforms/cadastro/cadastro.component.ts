c5af0e16a9ed99ba9b9d51c5619e471059900397
Felip3FL <felip3.fl@gmail.com>
2022-01-12 16:55:06 -0300

    FormControls e FormGroups

--- DAA/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
+++ DAA/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  cadastroForm123: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    //Aqui é para validar o controle individualmente, diferente do FormGroup
+    //let nome = new FormControl('');
+
+    //Aqui é para validação em grupo
+    //Posso tratar varios itens desse formulario, como se fosse o mesmo formulario
+    this.cadastroForm123 = new FormGroup({
+      nome12: new FormControl(''),
+      cpf12: new FormControl(''),
+      email12: new FormControl(''),
+      senha12: new FormControl(''),
+      senhaConfirmacao12: new FormControl(''),
+    });
+
+  }
+
+  adicionarUsuario(){
+    let x = this.cadastroForm123.value;
   }
 
 }
