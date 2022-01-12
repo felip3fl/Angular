39966e32d95093ca66f2dc2273d164f04fe2bd9d
Felip3FL <felip3.fl@gmail.com>
2022-01-12 16:11:45 -0300

    Criação novo projeto DAA

---
+++ DAA/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { rootRouterConfig } from './app.routes';
+import { CadastroComponent } from './demos/reactiveforms/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+  ],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
