9ccff73c171dc6bfe0fd9b8b8cce3caa8515695f
Felip3FL <felip3.fl@gmail.com>
2022-01-20 17:06:07 -0300

    Tratamento de erro 404

--- DAA/src/app/app.routes.ts
+++ DAA/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveforms/cadastro/cadastro.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -11,7 +12,10 @@ export const rootRouterConfig: Routes = [
     { path: 'cadastro', component: CadastroComponent },
     { path: 'produtos', 
         loadChildren: () => import('./demos/arquitetura-componentes/produto.module')
-        .then(m => m.ProdutoModule)}
+        .then(m => m.ProdutoModule)},
+
+    //ATENÇÃO, Esse linha SEMPRE tem que ser a última, se não irá bugar o resto da navegação
+    { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
