f83e8372c1febd4ad7963d0d22bee9dcfd3af3a6
Felip3FL <felip3.fl@gmail.com>
2022-01-21 09:06:34 -0300

    Rotas filhas em sub-módulos - Editar itens

--- DAA/src/app/demos/arquitetura-componentes/produto.route.ts
+++ DAA/src/app/demos/arquitetura-componentes/produto.route.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { EditarProdutoComponent } from "./editar-produto/editar-produto.component";
 import { ProdutoDashboardComponent } from "./produto-dashboard/produto-dashboard.component";
 
 const produtoRouterConfig: Routes=[
-    {path: '', component: ProdutoDashboardComponent}
+    {path: '', component: ProdutoDashboardComponent},
+    {path: 'editar/:id', component: EditarProdutoComponent}
 ];
 
 @NgModule({
