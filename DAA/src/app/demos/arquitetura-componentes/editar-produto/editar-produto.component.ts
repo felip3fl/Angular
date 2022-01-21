098d6b8fed1221ab63c7bbb8f5c5581f13cb9213
Felip3FL <felip3.fl@gmail.com>
2022-01-21 13:58:34 -0300

    Navegação imperativa com Router API

--- DAA/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ DAA/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -13,15 +13,24 @@ export class EditarProdutoComponent implements OnInit {
   produto: Produto;
 
   constructor(
-    //router: ActivatedRoute - aqui serve para pegar a Rota ativa
+    //route: ActivatedRoute - aqui serve para pegar a Rota ativa
     private route: ActivatedRoute, 
-    private produtoService: ProdutoService
+    private produtoService: ProdutoService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.produto = this.produtoService.obterPorId(params['id']);
     });
+  }
+
+  salvar(){
+    //fazer comunicação com backend ...........
+
+    this.router.navigate(['/produtos']);
+    //this.router.navigateByUrl(['/produtos']); //não indicado. recarrega a pagina inteira
+
   }
 
 }
