f40c3d1dc7565a2854576d11f06adeeddf88311c
Felip3FL <felip3.fl@gmail.com>
2022-01-21 13:54:22 -0300

    Fix route

--- DAA/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ DAA/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from '../models/produto';
 import { ProdutoService } from '../services/produto.service';
 
@@ -14,11 +14,12 @@ export class EditarProdutoComponent implements OnInit {
 
   constructor(
     //router: ActivatedRoute - aqui serve para pegar a Rota ativa
-    private router: ActivatedRoute, private produtoService: ProdutoService
+    private route: ActivatedRoute, 
+    private produtoService: ProdutoService
   ) { }
 
   ngOnInit() {
-    this.router.params.subscribe(params => {
+    this.route.params.subscribe(params => {
       this.produto = this.produtoService.obterPorId(params['id']);
     });
   }
