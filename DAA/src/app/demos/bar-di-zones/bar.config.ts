67cde7814f35d74434561bcf6ef3e4560190e6f3
Felip3FL <felip3.fl@gmail.com>
2022-01-28 12:08:43 -0300

    Atualização projeto

--- DAA/src/app/demos/bar-di-zones/bar.config.ts
+++ DAA/src/app/demos/bar-di-zones/bar.config.ts
@@ -5,5 +5,3 @@ export interface BarUnidadeConfig {
     unidadeId: number,
     unidadeToken: string
 }
-
-export const BAR_UNIDADE_CONFIG = new InjectionToken<BarUnidadeConfig>('BAR_UNIDADE_CONFIG');
