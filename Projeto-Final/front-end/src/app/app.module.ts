17b42525223ff53eafc93085a5fe508f75f5304a
Felip3FL <felip3.fl@gmail.com>
2022-02-16 16:34:09 -0300

    Customizando a navegação - Navegação basica

--- Projeto-Final/front-end/src/app/app.module.ts
+++ Projeto-Final/front-end/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavegacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
