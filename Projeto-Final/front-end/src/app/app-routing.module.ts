ee179c7492649a1647669c2f22f42e26ef425a47
Felip3FL <felip3.fl@gmail.com>
2022-03-02 11:57:33 -0300

    Base projeto fornecedor

--- Projeto-Final/front-end/src/app/app-routing.module.ts
+++ Projeto-Final/front-end/src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ const routes: Routes = [
     loadChildren: () => import('./conta/conta.module')
       .then(m => m.ContaModule)
   },
+  {
+    path: 'fornecedores',
+    loadChildren: () => import('./fornecedor/fornecedor.module')
+      .then(m => m.FornecedorModule)
+  },
   { path: 'nao-encontrado', component: NotFoundComponent },
   { path: '**', component: NotFoundComponent }
 ];
