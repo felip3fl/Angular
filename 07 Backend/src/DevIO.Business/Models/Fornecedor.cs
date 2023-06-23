572f3041d1287ba3b33d2a4d28f93d34cb9260ca
Felip3FL <felip3.fl@gmail.com>
2023-06-23 10:04:34 -0300

    Add backend project

---
+++ 07 Backend/src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevIO.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        #region | oi |
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        #endregion
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
